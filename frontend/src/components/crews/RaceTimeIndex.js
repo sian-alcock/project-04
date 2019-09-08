1fc1394a07a18de616ec47b99a2b3268e75f70ee
Sian Alcock <sian.alcock@lawena.org>
2019-09-08 19:19:27 +0100

    Started the RaceTime index page

--- frontend/src/components/crews/RaceTimeIndex.js
+++ frontend/src/components/crews/RaceTimeIndex.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class RaceTimeIndex extends React.Component {
   constructor() {
@@ -17,12 +17,45 @@ class RaceTimeIndex extends React.Component {
 
   render() {
 
-    console.log(this.state.crews)
+    console.log(this.state.race_times)
 
     return (
       <section className="section">
         <div className="container">
           <h1>RaceTime Index</h1>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Crew ID</th>
+                <th>Crew name</th>
+                <th>Sequence</th>
+                <th>Bib number</th>
+                <th>Start / Finish Tap</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Crew ID</th>
+                <th>Crew name</th>
+                <th>Sequence</th>
+                <th>Bib number</th>
+                <th>Start / Finish Tap</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              {this.state.race_times.map(raceTime =>
+                <tr key={raceTime.id}>
+                  <td><Link to={`/race_time/${raceTime.crew_id}`}>{raceTime.crew_id}</Link></td>
+                  <td>{raceTime.name}</td>
+                  <td>{raceTime.sequence}</td>
+                  <td>{raceTime.bib_number}</td>
+                  <td>{raceTime.time_tap}</td>
+
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
       </section>
     )
